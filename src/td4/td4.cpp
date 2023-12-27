cbac88798b10f1c958fc00cb0f52490b170ae06b
Lukasz Gesieniec <lukasz.gesieniec@gmail.com>
2023-12-27 14:50:57 +0100

    Feature 'assemble' implementation started

--- src/td4/td4.cpp
+++ src/td4/td4.cpp
@@ -1,2 +1,8 @@
 #include "td4.hpp"
 
+Td4::Status Td4::assemble(array<string, 16> in, array<string, 16> &out) {
+
+    out[0] = "00000100";
+
+    return Status::Ok;
+}
--- ut/td4_test.cpp
+++ ut/td4_test.cpp
@@ -23,3 +23,26 @@ TEST(Td4TestGroup, CreateTd4Object)
    Td4 cpu;
    (void)cpu;
 }
+
+TEST(Td4TestGroup, Td4AsmCall)
+{
+  array<string, 16> in{"IN A"};
+  array<string, 16> out;
+  Td4 td4asm;
+
+  Td4::Status status = td4asm.assemble(in, out);
+
+  CHECK_TRUE(Td4::Status::Ok == status);
+}
+
+TEST(Td4TestGroup, Td4AsmCheckOut)
+{
+  array<string, 16> in{"IN A"};
+  array<string, 16> out;
+  Td4 td4asm;
+
+  Td4::Status status = td4asm.assemble(in, out);
+
+  CHECK_TRUE(Td4::Status::Ok == status);
+  STRCMP_EQUAL((char *)"00000100", out[0].c_str());
+}
